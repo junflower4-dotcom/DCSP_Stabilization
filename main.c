ea4282ca9d19a14eb0e1a52a663a25b5fc6975aa
Yejun Lee <junflower4@gmail.com>
2026-07-15 10:36:05 +0900

    Update main() date

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 /*----------------------------------------------------------------\
-@ DCSP
+@ HGU_MCE_DCSP
 
-Title            : gimbal_6_StabilizationLoop
+Title            : gimbal_StabilizationLoop
 Author           : Yejun_Lee
 Created          : 10-06-2026
-Modified         : 10-06-2026
+Modified         : 15-07-2026
 
 ----------------------------------------------------------------*/
 
